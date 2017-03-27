c4a62822847a030990363129750a2ca1bf7a05b4
Ruan Odendaal <ruanodendaal@gmail.com>
2017-03-27 12:09:44 +0100

    refactore menu options

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,12 +1,24 @@
+# Restaurant / Menu class
+# loads menu options based on user selection
+
 class Menu
   attr_reader :dishes
 
-  def initialize
-    @dishes = {'Chicken' => 12.50,
-               'Pizza' => 10,
-               'Pasta'=> 8.99,
-               'Steak'=> 25.99}
+  def initialize(dishes)
+    @dishes = dishes
+    # {'Chicken' => 12.50,
+    #            'Pizza' => 10,
+    #            'Pasta'=> 8.99,
+    #            'Steak'=> 25.99}
   end
+
+  # let user choose menu, capture input and send to #load_menu
+  def print_menu
+    dishes.map do |item, price|
+      "%s £%.2f" % [item.to_s.capitalize, price]
+    end.join(", ")
+  end
+  #load_menu choice from csv
 
   def contain_item?(dish)
     self.dishes.include?(dish) ? true : "#{dish} not available in menu"
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,13 +1,31 @@
 require 'menu'
 
 describe Menu do
-  subject(:menu) { described_class.new }
+  subject(:menu) { described_class.new(dishes) }
   let(:dish) { double :dish }
   let(:price) { double :price }
+  let(:dishes) do
+    {
+      chicken: 12.50,
+      pizza: 10.00,
+      pasta: 8.99,
+      steak: 25.99
+    }
+  end
 
+  it 'has a list of dises with prices' do
+    expect(menu.dishes).to eq dishes
+  end
   describe '#initialization' do
-    it 'should create an empty menu array' do
+    it 'should create an empty menu hash' do
       expect(menu.dishes).to_not be_empty
+    end
+  end
+
+  describe '#print_menu' do
+    it 'prints the menu with prices' do
+      printed_menu = "Chicken £12.50, Pizza £10.00, Pasta £8.99, Steak £25.99"
+      expect(menu.print_menu).to eq printed_menu
     end
   end
 
